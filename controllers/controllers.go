0f7be2a801d733f66bc6f8cc49597397ad75cfb7
Datallboy <github@datallboy.com>
2023-01-20 15:55:37 -0500

    Refactor to use generalized HTTP requester

--- controllers/controllers.go
+++ controllers/controllers.go
@@ -36,61 +36,44 @@ func GetExtClientConf(config *configs.Config) echo.HandlerFunc {
 	}
 }
 
-type JsonBody struct {
-	ClientID string `json:"ClientID"`
-}
-
 // ROUTE: 			POST /clients CreateExtClient
 //
 // DESCRIPTION: Create a new extclient config. Clientid must be unique.
 
 func CreateExtClient(config *configs.Config) echo.HandlerFunc {
 	return func(c echo.Context) error {
-		jsonBody := new(JsonBody)
+
+		type reqBody struct {
+			ClientID string `json:"clientid"`
+		}
+
+		jsonBody := new(reqBody)
 
 		if err := c.Bind(&jsonBody); err != nil {
-			return c.JSON(http.StatusBadRequest, responses.HTTPResponse{Status: http.StatusBadRequest, Message: "error", Data: &echo.Map{"data": "Please specify ClientID."}})
+			return c.JSON(http.StatusBadRequest, responses.HTTPResponse{Status: http.StatusBadRequest, Message: "error", Data: &echo.Map{"data": "Please specify clientid in request body."}})
 		}
 
 		url := fmt.Sprintf("%s/api/extclients/clients/%s", config.NetmakerApiUrl, config.NetmakerIngressNodeID)
 		values := map[string]string{
 			"ClientID": jsonBody.ClientID,
 		}
-		reqBody, err := json.Marshal(values)
+		var response []byte
+
+		body, err := json.Marshal(values)
 
 		if err != nil {
-			log.Fatalf("Failed to marshal request body: %s", err)
+			log.Println(err)
+			return c.JSON(http.StatusInternalServerError, responses.HTTPResponse{Status: http.StatusInternalServerError, Message: "error", Data: &echo.Map{"data": "Error creating request body."}})
 		}
 
-		req, err := http.NewRequest(http.MethodPost, url, bytes.NewBuffer(reqBody))
-		if err != nil {
-			fmt.Print(err)
-			return c.JSON(http.StatusBadRequest, responses.HTTPResponse{Status: http.StatusBadRequest, Message: "error", Data: &echo.Map{"data": "Something went wrong."}})
+		_, appErr := utils.NewHTTPRequest(http.MethodPost, url, bytes.NewReader(body), config, response)
+		if appErr != nil {
+			return c.JSON(appErr.Code, responses.HTTPResponse{Status: appErr.Code, Message: "error", Data: &echo.Map{"data": appErr.Message}})
 		}
 
-		req.Header.Set("Authorization", fmt.Sprintf("Bearer %s", configs.MasterKey()))
-		req.Header.Set("Content-Type", "application/json")
-		resp, err := config.HttpClient.Do(req)
-		if err != nil {
-			fmt.Print(err)
-			return c.JSON(http.StatusBadRequest, responses.HTTPResponse{Status: http.StatusBadRequest, Message: "error", Data: &echo.Map{"data": "Something went wrong."}})
-		}
-
-		defer resp.Body.Close()
-
-		if resp.StatusCode != 200 {
-			var nmRes responses.NMResponse
-
-			err = json.NewDecoder(resp.Body).Decode(&nmRes)
-			if err != nil {
-				log.Fatalln(err)
-			}
-			return c.JSON(http.StatusOK, responses.HTTPResponse{Status: http.StatusOK, Message: "error", Data: &echo.Map{"data": nmRes}})
-		}
-
-		resBody, e := utils.GetExtClientConf(config, jsonBody.ClientID)
-		if e != nil {
-			return c.JSON(e.Code, responses.HTTPResponse{Status: e.Code, Message: "error", Data: &echo.Map{"data": e.Message}})
+		resBody, appErr := utils.GetExtClientConf(config, jsonBody.ClientID)
+		if appErr != nil {
+			return c.JSON(appErr.Code, responses.HTTPResponse{Status: appErr.Code, Message: "error", Data: &echo.Map{"data": appErr.Message}})
 		}
 
 		return c.JSON(http.StatusOK, responses.HTTPResponse{Status: http.StatusOK, Message: "success", Data: &echo.Map{"data": string(resBody)}})
--- utils/utils.go
+++ utils/utils.go
@@ -1,6 +1,7 @@
 package utils
 
 import (
+	"encoding/json"
 	"fmt"
 	"io"
 	"log"
@@ -14,43 +15,82 @@ import (
 
 func GetExtClientConf(config *configs.Config, clientid string) ([]byte, *responses.AppError) {
 	url := fmt.Sprintf("%s/api/extclients/clients/%s/file", config.NetmakerApiUrl, clientid)
+	var response []byte
 
-	req, err := NewHTTPRequest(http.MethodGet, url, nil)
-	if err != nil {
-		log.Println(err)
-		return nil, &responses.AppError{Error: err, Code: 500, Message: "Error creating HTTP request"}
-	}
-
-	resp, err := config.HttpClient.Do(req)
-	if err != nil {
-		log.Println(err)
-		return nil, &responses.AppError{Error: err, Code: 500, Message: "Error sending HTTP request"}
-	}
-
-	defer resp.Body.Close()
-
-	var nmRes responses.NMResponse
-	appErr := nmRes.HandleError(resp)
+	res, appErr := NewHTTPRequest("GET", url, nil, config, response)
 	if appErr != nil {
 		return nil, appErr
 	}
 
-	body, err := io.ReadAll(resp.Body)
-	if err != nil {
-		log.Println(err)
-		return nil, &responses.AppError{Error: err, Code: 500, Message: "Error decoding response body"}
-	}
-
-	return body, nil
+	return res, nil
 }
 
-// DESCRIPTION:	returns http request with Auth header
-func NewHTTPRequest(method string, url string, body io.Reader) (*http.Request, error) {
+// DESCRIPTION:	returns http response
+func NewHTTPRequest[T []byte](method string, url string, body io.Reader, config *configs.Config, responseType T) (T, *responses.AppError) {
 	req, err := http.NewRequest(method, url, body)
 	if err != nil {
-		return nil, err
+		log.Println(err)
+		return responseType, &responses.AppError{Error: err, Code: 500, Message: "Error creating HTTP request"}
 	}
 
+	// set Netmaker Auth header for all requests
 	req.Header.Set("Authorization", fmt.Sprintf("Bearer %s", configs.MasterKey()))
-	return req, nil
+
+	// set content type for POST requests
+	if method == http.MethodPost {
+		req.Header.Set("Content-Type", "application/json")
+	}
+
+	// log the request
+	log.Printf("%s %s\n", method, req.URL.String())
+
+	res, err := config.HttpClient.Do(req)
+	if err != nil {
+		log.Println(err)
+		return responseType, &responses.AppError{Error: err, Code: 500, Message: "Error sending HTTP request"}
+	}
+
+	defer res.Body.Close()
+
+	// handle Netmaker error
+	if res.StatusCode != http.StatusOK {
+		var nmRes responses.NetmakerErrorResponse
+
+		err := json.NewDecoder(res.Body).Decode(&nmRes)
+		if err != nil {
+			log.Println(err)
+			return responseType, &responses.AppError{Error: err, Code: 500, Message: "Error decoding response body"}
+		}
+
+		if nmRes.Message == "no result found" {
+			return responseType, &responses.AppError{Error: nil, Code: 404, Message: nmRes.Message}
+		}
+		return responseType, &responses.AppError{Error: nil, Code: nmRes.Code, Message: nmRes.Message}
+	}
+
+	// Netmaker returns raw text for config requests
+	if res.Header.Get("Content-Type") != "application/json" {
+		body, err := io.ReadAll(res.Body)
+		if err != nil {
+			log.Println(err)
+			return responseType, &responses.AppError{Error: err, Code: 500, Message: "Error reading response body"}
+		}
+
+		return body, nil
+	}
+
+	var responseObject T
+	err = json.NewDecoder(res.Body).Decode(&responseObject)
+
+	// Netmaker sometimes returns an empty body
+	if err == io.EOF {
+		return responseObject, nil
+	}
+
+	if err != nil {
+		log.Println(err)
+		return responseType, &responses.AppError{Error: err, Code: 500, Message: err.Error()}
+	}
+
+	return responseObject, nil
 }
